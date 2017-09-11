cc7fef59522847bc4a4ad628c651f444e710b3d3
Gjert Ingar Gjersund <gjertig@stud.ntnu.no>
2017-09-12 01:36:20 +0200

    Final update of the day

--- api/controllers/account/AccountSignInController.php
+++ api/controllers/account/AccountSignInController.php
@@ -12,21 +12,25 @@ class AccountSignInController {
   }
 
   public function __invoke(Request $request, Response $response) {
-
     $uri = $request->getUri();
+    $data = [];
+    
+    # Check input
 
+    # Check bruteforce
+
+    # If ok -> Sign in
     $jwtManager = new JWTManager($this->container->get('settings')['jwtSecret']);
     $jwt = $jwtManager->create($uri->getBaseUrl(), 123, time() + 60);
-    $data = [
-      'href' => $uri->getBaseUrl().$uri->getPath(),
-      'timestamp' => time(),
-      'status' => $response->getStatusCode(),
-      'message' => '',
-      'developer' => '',
-      'token' => $jwt
-    ];
+    $data['token'] = $jwt;
+    $data['message'] = 'Success';
+    $data['developer'] = 'User is signed in';
+    $data['status'] = 200;
 
-    # 'decoded' => $this->get("jwt")
+    # Else -> Error
+
+    $data['href'] = $uri->getBaseUrl().$uri->getPath();
+    $data['timestamp'] = time();
 
     return $response->withJson($data);
   }
